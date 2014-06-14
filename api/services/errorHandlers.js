5b86d11694878ed70411e20939e587d94a0370c7
Will Ricketts <will@willricketts.com>
2014-06-14 00:11:45 -0400

    Added first implementation of error logging

--- api/services/errorHandlers.js
+++ api/services/errorHandlers.js
@@ -2,7 +2,18 @@ module.exports = {
 
   serverError: function(res, err){
     if(err){
-      return res.send(500, 'Lost in space!');
+      Log.create({ content: err }, function(err, error){
+        //this seems dumb.
+        return res.send(500, 'Lost in space!');
+      });
+    }
+  },
+
+  notFound: function(res, collection){
+    if(collection === undefined){
+      Log.create({ content: 'Query for "' + collection + '" returned undefined' }, function(err, log){
+        return res.send(404);
+      });
     }
   }
 }
