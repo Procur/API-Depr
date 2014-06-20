33383b1e3c0d8463b1d805d46f3bba21ef7506bc
Will Ricketts <will@willricketts.com>
2014-06-20 12:41:41 -0400

    moved deprecated actions in userFunctions.js

--- api/services/models/userFunctions.js
+++ api/services/models/userFunctions.js
@@ -1,17 +1,7 @@
 var handler = require('../errorHandlers.js');
 
 module.exports = {
-  /*findByApiToken: function(res, token, callback){
-    ApiToken.findOne({ token: token }, function(err, token){
-      handler.serverError(res, err);
-      if(token === undefined){ return res.send(400, 'Invalid API token'); };
-      User.findOne({ id: token.user }, function(err, user){
-        handler.serverError(res, err);
-        callback(err, user);
-      });
-    });
-  },*/ //DEPRECATED
-
+  
   findByApiToken: function(req, res, callback){
     var token = req.headers.apitoken;
     ApiToken.findOne({ token: token }, function(err, token){
@@ -38,3 +28,19 @@ module.exports = {
   }
 };
 
+/* ////  DEPRECATED ACTIONS  ////
+
+findByApiToken: function(res, token, callback) {
+  ApiToken.findOne({ token: token }, function (err, token) {
+    handler.serverError(res, err);
+    if (token === undefined) {
+      return res.send(400, 'Invalid API token');
+    };
+    User.findOne({ id: token.user }, function (err, user) {
+      handler.serverError(res, err);
+      callback(err, user);
+    });
+  });
+}
+
+ */
