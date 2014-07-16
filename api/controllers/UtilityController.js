df42f1930463343493a7b91e50411a286fd84785
Will Ricketts <will@willricketts.com>
2014-07-16 13:05:03 -0400

    added heartbeat

---
+++ api/controllers/UtilityController.js
@@ -0,0 +1,12 @@
+/**
+ * UtilityController
+ *
+ * @description :: Server-side logic for managing utilities
+ * @help        :: See http://links.sailsjs.org/docs/controllers
+ */
+
+module.exports = {
+	index: function(req, res){
+		res.send("OK", 200);
+	}
+};
